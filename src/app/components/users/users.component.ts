1353032b5c25c821aa335ceff404f9aaa66a5eff
Abayomi <eyitouyoa3@gmail.com>
2021-12-16 13:32:42 -0500

    code clean up

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UsersService } from '../../services/users.service';
 import { User } from '../../interfaces/IUser';
+import { catchError, takeUntil } from 'rxjs/operators';
+import { of, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, OnDestroy {
   public users: User[];
+  private onDestroy$: Subject<void> = new Subject<void>();
 
   constructor(private usersService: UsersService) { }
 
@@ -16,8 +19,15 @@ export class UsersComponent implements OnInit {
     this.loadUsers();
   }
 
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+  }
+
   private loadUsers() {
-    this.usersService.getUsers().subscribe((data: User[]) => {
+    this.usersService.getUsers().pipe(takeUntil(this.onDestroy$), catchError((err: any) => {
+      console.log(err);
+      return of([]);
+    })).subscribe((data: User[]) => {
       this.users = data
     })
   }
