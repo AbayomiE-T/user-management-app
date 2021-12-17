e59ddef9c1534c6115b7b236f09c32ae0e0454b5
Abayomi <eyitouyoa3@gmail.com>
2021-12-17 12:50:57 -0500

    added a navigation bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddUserComponent } from './components/users/add-user/add-user.component';
 import { UserDetailsComponent } from './components/users/user-details/user-details.component';
 import { UsersComponent } from './components/users/users.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/users', pathMatch: 'full' },
   { path: 'users', component: UsersComponent },
-  { path: 'users/:id', component: UserDetailsComponent }
+  { path: 'users/:id', component: UserDetailsComponent },
+  { path: 'create', component: AddUserComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { UserDetailsComponent } from './components/users/user-details/user-details.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { AddUserComponent } from './components/users/add-user/add-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    UserDetailsComponent
+    UserDetailsComponent,
+    NavigationComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule,
